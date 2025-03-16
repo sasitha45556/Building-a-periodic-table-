2ad4b1f62eb7511cd2ba6e9524ee1dbfe1a276ef
Sasitha Ratnayake <ratnayakecjw@yahoo.com>
2025-03-16 03:50:10 +0000

    chore: newer code

--- element.sh
+++ element.sh
@@ -12,3 +12,7 @@ fi
 # Query database based on argument type
 if [[ $1 =~ ^[0-9]+$ ]]
 then
+RESULT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number = $1")
+else
+  RESULT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE name = '$1' OR symbol = '$1'")
+fi
