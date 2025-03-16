30beba7d35bb37e49f88b8aca582d73a4b16afd2
Sasitha Ratnayake <ratnayakecjw@yahoo.com>
2025-03-16 03:49:28 +0000

    fix: new code

--- element.sh
+++ element.sh
@@ -4,3 +4,11 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 # Check for arguments
 if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+  exit
+fi
+
+# Query database based on argument type
+if [[ $1 =~ ^[0-9]+$ ]]
+then
