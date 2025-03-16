90d8450a9b288b4bc4d42958c8da5b8f82ba4c72
Sasitha Ratnayake <ratnayakecjw@yahoo.com>
2025-03-16 03:51:02 +0000

    test: add code

--- element.sh
+++ element.sh
@@ -16,3 +16,14 @@ RESULT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_p
 else
   RESULT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE name = '$1' OR symbol = '$1'")
 fi
+
+# Display result or error message
+if [[ -z $RESULT ]]
+then
+  echo "I could not find that element in the database."
+else
+  echo "$RESULT" | while IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING
+  do
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+  done
+fi
